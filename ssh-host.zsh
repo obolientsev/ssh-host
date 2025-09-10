e33f18c7485f67635ca0d17c01a7c4e244fce56a
Obolientsev <sl.dambo@gmail.com>
2025-09-10 13:22:15 +0200

    feat: add dependency checking

--- ssh-host.zsh
+++ ssh-host.zsh
@@ -5,5 +5,20 @@ source "${SSH_HOST_PLUGIN_DIR}/ssh-host.plugin.conf"
 source "${0:A:h}/src/_ssh_host_main"
 
 ssh-host() {
-  _ssh_host_main "$@"
+  _ssh_host_check_dependencies && _ssh_host_main "$@"
+}
+
+_ssh_host_check_dependencies() {
+    local missing_deps=()
+    local deps=(fzf ssh-keygen ssh)
+
+    for dep in "${deps[@]}"; do
+       command -v "$dep" >/dev/null || missing_deps+=("$dep")
+    done
+
+    [[ ${#missing_deps[@]} -eq 0 ]] && return 0
+
+    echo "Missing required dependencies:"
+    printf '  - %s\n' "${missing_deps[@]}"
+    return 1
 }
